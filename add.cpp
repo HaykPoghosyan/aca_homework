1ead58e1a738d74d3f19433c548ec396ed3e8569
HaykPoghosyan <hayk.poghosyan.2016@gmail.com>
2023-06-04 15:03:27 +0400

    add makefile add.cpp add.h modify main.cpp

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+
+
+int add(int a, int b, int c){
+
+	std::cout << a << " " << b << " " << c << std::endl;
+	return a + b + c;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include "add.h"
 
 
 int main(){
 
 	std::cout << "Hello world" << std::endl;
+	add(3,4,5);
 	return 0;
 }
